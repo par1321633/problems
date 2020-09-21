0cc9f66aa4577b0c3c368798de6e26dec1dc5e7f
Parkash Sharma <Parkash.Sharma@in.ey.com>
2020-09-21 21:57:09 +0530

    Best Time to Buy and Sell Stock and Car Pooling Prob

---
+++ leetcode_september_challenge/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+__author__ = "Parkash Sharma"
+
+"""
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Note that you cannot sell a stock before you buy one.
+
+Example 1:
+Input: [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+             Not 7-1 = 6, as selling price needs to be larger than buying price.
+
+Example 2:
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+
+"""
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_index = 0
+        max_index = 0
+        max_profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] < prices[min_index]:
+                min_index = i
+                max_index = i
+            if prices[i] > prices[max_index]:
+                max_index = i
+                profit = prices[max_index] - prices[min_index]
+                if profit > max_profit:
+                    max_profit = profit
+        return max_profit
+
+
+if  __name__ == '__main__':
+    print ("Running Best Time to Buy and Sell Stock Program")
+    prices = [7, 1, 5, 3, 6, 4]
+    print ("Case 1 : {}".format(prices))
+    print (Solution().maxProfit(prices))
+    prices = [7, 6, 4, 3, 1]
+    print("Case 2 : {}".format(prices))
+    print(Solution().maxProfit(prices))
---
+++ leetcode_september_challenge/car-pooling.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+__author__ = "Parkash Sharma"
+
+"""
+
+You are driving a vehicle that has capacity empty seats initially available for passengers.  The vehicle only drives east (ie. it cannot turn around and drive west.)
+
+Given a list of trips, trip[i] = [num_passengers, start_location, end_location] contains information about the i-th trip: the number of passengers that must be picked up, and the locations to pick them up and drop them off.  The locations are given as the number of kilometers due east from your vehicle's initial location.
+
+Return true if and only if it is possible to pick up and drop off all passengers for all the given trips.
+
+Example 1:
+Input: trips = [[2,1,5],[3,3,7]], capacity = 4
+Output: false
+
+Example 2:
+Input: trips = [[2,1,5],[3,3,7]], capacity = 5
+Output: true
+
+Example 3:
+Input: trips = [[2,1,5],[3,5,7]], capacity = 3
+Output: true
+
+Example 4:
+Input: trips = [[3,2,7],[3,7,9],[8,3,9]], capacity = 11
+Output: true
+"""
+
+from typing import List
+
+
+class Solution:
+
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        passenger_count = {}
+        for i in trips:
+            cap, start, end = i
+            for k in range(start + 1, end + 1):
+                passenger_count[k] = passenger_count.get(k, 0) + cap
+                if passenger_count[k] > capacity:
+                    return False
+
+        return True
+
+
+if __name__ == '__main__':
+    print ("Running Car Pooling Program")
+    trips = [[2, 1, 5], [3, 3, 7]]
+    capacity = 4
+    print("Case 1 : Trips : {}, Capacity : {}".format(trips, capacity))
+    print (Solution().carPooling(trips, capacity))
+
+    trips = [[2, 1, 5], [3, 3, 7]]
+    capacity = 5
+    print("Case 2 : Trips : {}, Capacity : {}".format(trips, capacity))
+    print (Solution().carPooling(trips, capacity))
+
+    trips = [[2, 1, 5], [3, 5, 7]]
+    capacity = 3
+    print("Case 3 : Trips : {}, Capacity : {}".format(trips, capacity))
+    print (Solution().carPooling(trips, capacity))
+
+    trips = [[3, 2, 7], [3, 7, 9], [8, 3, 9]]
+    capacity = 11
+    print("Case 4 : Trips : {}, Capacity : {}".format(trips, capacity))
+    print (Solution().carPooling(trips, capacity))
